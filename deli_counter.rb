3d02d1f08edfbd81389c43f8b0e1bca7f54ad29a
Sam D <samidiaz3@gmail.com>
2021-01-31 19:56:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,21 +7,15 @@ def line (array)
   if array.length == 0
     puts "The line is currently empty."
   else
-#iterate
-#array.each.with_index(1).map do |customer, number|
-      #  "#{number}: #{customer}"
-  #end
-
-# need to add the index no
-new_array = array.each.with_index(1).map do |customer, number|
-        "#{number}. #{customer}"
+    # need to add the index no. to array elements
+    array_with_index = array.each.with_index(1).map do |customer, number|
+      "#{number}. #{customer}"
+    end
+    # string the array
+    string_line = array_with_index.join(" ")
+    # put the phrase
+    puts "The line is currently: #{string_line}"
   end
-# string the array
-string_line = new_array.join(" ")
-# put the phrase
-puts "The line is currently: #{string_line}"
-
-end
 end
 
 # array = ["Logan", "Avi", "Spencer"]
