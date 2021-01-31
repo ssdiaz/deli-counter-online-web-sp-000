e381b12805bf0546e8a03bf66656dfcd066a53c5
Sam D <samidiaz3@gmail.com>
2021-01-31 19:26:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+katz_deli = []
+
+#shows everyone in current place in line
+def line (array)
+  if line == 0
+    return "The line is currently empty."
+  else
+    array.length
+
+
+  end
+end
