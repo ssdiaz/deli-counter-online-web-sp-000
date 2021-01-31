791b6aad6e82735fd75d23a97540b8c971b45573
Sam D <samidiaz3@gmail.com>
2021-01-31 20:23:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,13 @@
 # Write your code here.
-
 #katz_deli = []
 
 #shows everyone in current place in line
-def line (array)
-  if array.length == 0
+def line (katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     # need to add the index no. to array elements
-    new_array_numbered= array.each.with_index(1).map do |customer, number|
+    new_array_numbered= katz_deli.each.with_index(1).map do |customer, number|
       "#{number}. #{customer}"
     end
     # string the array
@@ -23,11 +22,14 @@ end
 # new_customer = "sam"
 
 def take_a_number (katz_deli, new_customer)
+
   katz_deli << new_customer
 
   array_with_index = katz_deli.each.with_index(1).map do |new_customer, number|
       puts "#{number}. #{new_customer}"
   end
+
+new_array = array_with_index
 
   puts array_with_index[-1]
 
